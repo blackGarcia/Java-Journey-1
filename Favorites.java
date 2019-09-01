45d29e0a07026eb110d2fdeda560258b0cc0d72e
Rita Jordan <rjordan.tenth@gmail.com>
2019-09-01 16:24:49 -0400

    Add files via upload

---
+++ Favorites.java
@@ -0,0 +1,25 @@
+/*Class:	CSE 1321L
+Section: 	W01          
+Term:		Fall 2019  
+Instructor:	Rehnuma Afrin
+Name:		Sarita Jordan    
+Lab#:		2
+*/
+public class Favorites {
+
+	public static void main (String[] args)
+	{
+		String name = "Sarita Jordan";
+		String birthday = "12/21/1987"; 
+		String hobby1 = "reading, ";
+		String hobby2 = "listening to music, "; 
+		String hobby3 = "art viewings";
+		String book = "The Alchemist";
+		String movie = "The Fault In Our Stars";
+		System.out.println ("My name: " + name);
+		System.out.println ("My birthday: " + birthday);
+		System.out.println ("My hobbies: " + hobby1 + hobby2 + "and " + hobby3);
+		System.out.println ("My favorite book: " + book);
+		System.out.println ("My favorite movie:" + movie);
+		}
+}
